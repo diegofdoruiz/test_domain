999769c848331c64211c3bb262e41895256186c4
diegopccali1 <dieg.fernando.ruiz@correounivalle.edu.co>
2019-02-16 14:23:28 -0500

    vista

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -83,30 +83,36 @@ class FrontEndController extends Controller
     }
 
     //Manejar dominios externos a la plataforma
-    public function outDomain($domain, $page=NULL)
-    {
-        return view('inicio', [ 
-            'type' => 'external_domain',
-            'domain' => $domain,
-        ]);
-    }
-
-    //Manejar subdominios con dominios externos a la plataforma
-    public function outSubDomain($subdomain, $domain, $page=NULL)
-    {
-        return view('inicio', [ 
-            'type' => 'external_subdomain_domain',
-            'subdomain' => $subdomain,
-            'domain' => $domain,
-        ]);
-    }
-
-    //Manejar dominios externos a la plataforma
     public function inSubDomain($subdomain, $page=NULL)
     {
         return view('inicio', [ 
             'type' => 'internal_subdomain',
             'subdomain' => $subdomain,
         ]);
+    }
+
+    //Manejar dominios externos a la plataforma
+    public function outDomain($domain, $page=NULL)
+    {
+        $array_url = explode('.', $domain);
+        if(count($array_url) === 2){
+            return view('inicio', [ 
+                'type' => 'external_domain',
+                'domain' => $array_url[0].'.'.$array_url[1],
+            ]);
+        }else if(count($array_url) === 3){
+            if($array_url[0] === 'www'){
+                return view('inicio', [ 
+                    'type' => 'external_domain',
+                    'domain' => $array_url[0].'.'.$array_url[1].'.'.$array_url[2],
+                ]);
+            }else{
+                return view('inicio', [ 
+                    'type' => 'external_subdomain_domain',
+                    'subdomain' => $array_url[0],
+                    'domain' => $array_url[1].'.'.$array_url[2],
+                ]);
+            }
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -30,29 +30,9 @@ Route::group(['domain' => '{subdomain}.cohett.tk'], function(){
     Route::get('/{page?}', 'FrontEndController@inSubDomain');
 });
 
-
-Route::group(['domain' => '{domain}'], function(){
-	Route::get('/', function($domain){
-		$array_url = explode('.', $domain);
-		if(count($array_url) === 2){
-			return '<p>Está accediendo desde un dominio externo a la plataforma</p>
-				   <p>Dominio: '.$domain.'</p>';
-		}else if(count($array_url) === 3){
-			return '<p>Está accediendo desde un dominio con subdominio externo a la plataforma</p>
-				    <p>SubDominio: '.$array_url[0].'</p>
-				    <p>Dominio: '.$array_url[1].'.'.$array_url[2].'</p>';
-		}
-	});
-
-});
-
-/*Route::group(['domain' => '{subdomain}.app.{domain}'], function(){
-    Route::get('/{page?}', 'FrontEndController@outSubDomain');
-});
-
 Route::group(['domain' => '{domain}'], function(){
     Route::get('/{page?}', 'FrontEndController@outDomain');
-});*/
+});
 
 Auth::routes();
 
